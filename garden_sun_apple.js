c8ae33cfb1de6a8f25a4f4696ee6bcf2e2296d57
KueblerAbigeal1994 <137503587+KueblerAbigeal1994@users.noreply.github.com>
2023-06-25 08:14:27 +0700

    Add error handling for edge cases

---
+++ garden_sun_apple.js
@@ -0,0 +1,70 @@
+//I. Introduction 
+
+//1. Definition
+var musicSchool = {
+	//The music school provides quality music education and performance opportunities for students of all ages.
+	name: "Music Academy", 								
+	mission: "To share the gift of music with learners of all ages and to provide quality music education and performance opportunities.",
+	ages: "All ages",
+	
+	//2. Overview
+	courses: ["Music Theory", "Instument Performance", "Sight-Reading", "Composition", "Ear Training"],
+	locations: ["New York", "Los Angeles", "Chicago"],
+	teachers: ["In-House Teachers", "Guest Artists"],
+	headTeacher: "Mr. Smith"
+};
+
+//3. Partnerships
+var partners = ["Music Store", "Conservatory", "Theater Group"];
+
+//4. Programs
+var programs = {
+	//Youth
+	"Youth": {
+		description: "Offers comprehensive music education program to students aged 4-18.",
+		courses: ["Group Lessons", "Individual Lessons", "Performance Opportunities", "Summer Camps"],
+	}, 
+	//Adult
+	"Adult": {
+		description: "Offers comprehensive music education program to students aged 18+.",
+		courses: ["Group Lessons", "Individual Lessons", "Performance Opportunities", "Ensembles"],
+	},
+	//Summer
+	"Summer": {
+		description: "Offers comprehensive music education program to students aged 4-18 during the summer months.",
+		courses: ["Group Lessons", "Individual Lessons", "Performance Opportunities", "Summer Camps"]
+	}
+};
+
+//5. Events 
+var events = ["Recitals", "Competitions", "Workshops", "Social Gatherings"];
+
+//6. Resources 
+var resources = {
+	//Instructions
+	"Instructions": {
+		instructions: ["Online Tutorials", "Practice Tips"], 
+	},
+	//Recordings 
+	"Recordings": {
+		recordings: ["Student Recordings", "Teacher Recordings", "Performances"], 
+	},
+	//Curriculum 
+	"Curriculum": {
+		curriculum: ["Lesson Plans", "Syllabi", "Worksheets"], 
+	},
+	//Gear 
+	"Gear": {
+		gear: ["Instruments", "Music Books", "Music Stands"], 
+	}
+};
+
+//7. Contact 
+var contact = {
+	phone: "+1 500-777-7777", 
+	address: "123 Main Street, New York, NY 11111", 
+	email: "info@musicacademy.com"
+};
+ 
+//II. Summary 
+//The Music Academy provides quality music education and performance opportunities for students of all ages. We partner with music stores, conservatories, and theater groups to offer a comprehensive range of courses and programs, including youth, adult, and summer programs. Furthermore, we can provide resources to students like online tutorials and lesson plans, as well as student and teacher recordings of performances. Finally, we host events, such as recitals, competitions, workshops, and social gatherings. If you have any questions about our services, please contact us at +1 500-777-7777 or info@musicacademy.com.
